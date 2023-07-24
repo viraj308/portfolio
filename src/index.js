43794eb923e30a2cb5de5c6c7c122d6be59b6ccf
Viraj Chavan <virajchavan308@gmail.com>
2023-07-24 22:47:56 +0530

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
