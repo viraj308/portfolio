e921c3969f9500145fe293b19824dca993c2143f
Viraj Chavan <virajchavan308@gmail.com>
2023-07-26 00:27:01 +0530

    about page done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,35 @@
+import "./about.css";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="about-wrapper">
+        <div className="container">
+          <h1 className="about-heading">About Me</h1>
+          <p className="about-para">
+            Highly motivated beginner front-end developer .Started learning
+            web-dev a couple months back from a book. Driven and permanently
+            curious, inspired by the numerous intuitive web designs over
+            internet. A third year university student. A big fan of Anime, manga
+            and a beginner japanese language student
+          </p>
+          <hr className="line" />
+          <div className="social-icons">
+            <a href="#">
+              <AiFillTwitterCircle size={40} color="white" />
+            </a>
+            <a href="#">
+              <AiFillInstagram size={40} color="white" />
+            </a>
+            <a href="#">
+              <AiFillLinkedin size={40} color="white" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
